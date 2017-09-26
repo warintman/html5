48b6eabf70e8023803952307c8be3ec0f06e698c
warintman <warintman@gmail.com>
2017-09-26 11:21:56 +0200

    animaciones
    arreglo de animaciones

---
+++ default.js
@@ -0,0 +1,4 @@
+(function(){ 
+    document.getElementById('UserTool').addEventListener("mouseover",function over(){document.getElementById('ToolBar').style.display='block';});
+    document.getElementById('UserTool').addEventListener("mouseout",function over(){document.getElementById('ToolBar').style.display='none';});
+})();
