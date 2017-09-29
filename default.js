d714d713f69cf42860ef10e670481e58bb1803fe
warintman <warintman@gmail.com>
2017-09-29 13:20:26 +0200

    toolbar
    toolbar

--- default.js
+++ default.js
@@ -1,6 +1,6 @@
 (function(){ 
     //barra de navegacion (perfil + usuario)
-    document.getElementById('UserTool').addEventListener("mouseover",function mouseover(){document.getElementById('ToolBar').style.display='flex';});
+    document.getElementById('UserTool').addEventListener("mouseover",function mouseover(){document.getElementById('ToolBar').style.display='block';});
     document.getElementById('UserTool').addEventListener("mouseout",function mouseover(){document.getElementById('ToolBar').style.display='none';});
     //salida de la aplicacion
     document.getElementById('CloseSesionTool').addEventListener("click",function logout(){location.href = window.location.origin + '/' + window.location.pathname.split("/")[1] + '/default.aspx?action=logout';});
