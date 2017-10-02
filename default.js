35118cbdecd4c0da52760469f2119c49315a4a0f
warintman <warintman@gmail.com>
2017-10-02 14:23:42 +0200

    arreglos y menu con media
    arreglos y menu con media

--- default.js
+++ default.js
@@ -4,4 +4,37 @@
     document.getElementById('UserTool').addEventListener("mouseout",function mouseover(){document.getElementById('ToolBar').style.display='none';});
     //salida de la aplicacion
     document.getElementById('CloseSesionTool').addEventListener("click",function logout(){location.href = window.location.origin + '/' + window.location.pathname.split("/")[1] + '/default.aspx?action=logout';});
+    //el menu
+    document.getElementById('menuOpen').addEventListener("click",function openMenu(){
+        document.getElementById('menuOpen').style.display='none';
+        document.getElementById('menuClose').style.display='block';
+        document.getElementById('menu').classList.remove('menuHidden');        
+        document.getElementById('menu').classList.add('menuVisible');
+    });
+    document.getElementById('menuClose').addEventListener("click",closeMenu);
+
+    function closeMenu(){
+        document.getElementById('menuOpen').style.display='block';
+        document.getElementById('menuClose').style.display='none';
+        document.getElementById('menu').classList.remove('menuVisible');
+        document.getElementById('menu').classList.add('menuHidden');
+    }
+
+    var handleMediaChange = function (mql) {
+
+        // For some reason Firefox has trouble always running this code.
+        // The console.log seems to help it.
+        // TODO: Figure out what the hell that's all about
+        console.log();
+
+        if (mql.matches && (mql.media==='screen and (max-width: 660px)')) {
+            //si estaba abierto el menu, lo cierro
+            closeMenu();
+        }        
+    };
+
+    //https://bugs.chromium.org/p/chromium/issues/detail?id=49001
+    //poner el chrome con --allow-file-access-from-files para probarlo en local
+    mqEvents(handleMediaChange);
+
 })();
---
+++ mq-events.js
@@ -0,0 +1,32 @@
+(function () {
+    var mqEvents = function (mediaChangeHandler) {
+        var sheets = document.styleSheets;
+
+        // Use the provided handler if present
+        if (!mediaChangeHandler) {
+            console.error('mqEvents necesita mediaChangeHandler.');
+            return false;
+        }
+
+        if (sheets.length>0) {
+            for (var i = 0; i < sheets.length; i += 1) {
+                var rules = sheets[i].cssRules;
+                if (rules && rules.length>0) {
+                    for (var j = 0; j < rules.length; j += 1) {
+                        // This Stackoverflow answer helped me figure out how
+                        // to check the type of object each rule was
+                        // http://stackoverflow.com/a/332429/368634
+                        if (rules[j].constructor === CSSMediaRule) {
+                            var mql = window.matchMedia(rules[j].media.mediaText);
+                            mql.addListener(mediaChangeHandler);
+                            mediaChangeHandler(mql);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Yeah, this is a little shitty
+    window.mqEvents = mqEvents;
+}());
